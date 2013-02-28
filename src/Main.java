78cf2a5dea71f65249413c619922352452880d3a
Christoffer Henriksson <christoffer_henriksson@hotmail.com>
2013-02-28 14:08:19 +0100

    Lade till Main-klass

---
+++ src/Main.java
@@ -0,0 +1,14 @@
+
+public class Main {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		MainView mainView = new MainView();
+		mainView.setSize(1024, 770);
+		mainView.setLocationRelativeTo(null);
+		mainView.setVisible(true);
+	}
+
+}
