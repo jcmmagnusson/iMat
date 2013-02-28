0f79a69b53c00a0181a075db985aba21724cffc6
Johan Magnusson <jcmmagnusson@gmail.com>
2013-02-28 18:20:07 +0100

    La till lite olika paneler

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -7,6 +7,11 @@ import java.awt.event.MouseAdapter;
 import java.awt.event.MouseEvent;
 import java.awt.event.ActionListener;
 import java.awt.event.ActionEvent;
+import javax.swing.JPanel;
+import javax.swing.GroupLayout;
+import javax.swing.GroupLayout.Alignment;
+import javax.swing.LayoutStyle.ComponentPlacement;
+import java.awt.CardLayout;
 
 
 public class CheckOut extends JDialog {
@@ -32,21 +37,75 @@ public class CheckOut extends JDialog {
 	 * Create the dialog.
 	 */
 	public CheckOut() {
-		setBounds(100, 100, 508, 378);
+		setBounds(100, 100, 456, 378);
+		getContentPane().setLayout(new BorderLayout(0, 0));
 		
-		JButton btnOmgSoPhat = new JButton("OMG SO PHAT");
-		getContentPane().add(btnOmgSoPhat, BorderLayout.CENTER);
+		JPanel northPanel = new JPanel();
+		getContentPane().add(northPanel, BorderLayout.NORTH);
 		
+		JPanel panel = new JPanel();
 		
-		JButton btnDisablePhatPls = new JButton("disable phat pls");
-		btnDisablePhatPls.addActionListener(new ActionListener() {
-			public void actionPerformed(ActionEvent arg0) {
-				JButton btn = (JButton)arg0.getSource();
-				btn.setVisible(false);
-			}
-		});
-		getContentPane().add(btnDisablePhatPls, BorderLayout.WEST);
+		JPanel panel_1 = new JPanel();
+		
+		JPanel panel_2 = new JPanel();
+		
+		JPanel panel_3 = new JPanel();
+		GroupLayout gl_northPanel = new GroupLayout(northPanel);
+		gl_northPanel.setHorizontalGroup(
+			gl_northPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_northPanel.createSequentialGroup()
+					.addGap(80)
+					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
+					.addGap(80))
+		);
+		gl_northPanel.setVerticalGroup(
+			gl_northPanel.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_northPanel.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_northPanel.createParallelGroup(Alignment.LEADING)
+						.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		northPanel.setLayout(gl_northPanel);
+		
+		JPanel panel_4 = new JPanel();
+		getContentPane().add(panel_4, BorderLayout.SOUTH);
+		
+		JButton btnBakt = new JButton("Bak\u00E5t");
+		
+		JButton btnNsta = new JButton("N\u00E4sta");
+		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
+		gl_panel_4.setHorizontalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGap(80)
+					.addComponent(btnBakt)
+					.addPreferredGap(ComponentPlacement.RELATED, 145, Short.MAX_VALUE)
+					.addComponent(btnNsta)
+					.addGap(80))
+		);
+		gl_panel_4.setVerticalGroup(
+			gl_panel_4.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_4.createSequentialGroup()
+					.addGroup(gl_panel_4.createParallelGroup(Alignment.BASELINE)
+						.addComponent(btnBakt)
+						.addComponent(btnNsta))
+					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
+		);
+		panel_4.setLayout(gl_panel_4);
+		
+		JPanel panel_5 = new JPanel();
+		getContentPane().add(panel_5, BorderLayout.CENTER);
+		panel_5.setLayout(new CardLayout(0, 0));
 
 	}
-
 }
