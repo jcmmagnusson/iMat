ed0eafce6fd8151d636548902c2a44fce097fe1e
Johan Magnusson <jcmmagnusson@gmail.com>
2013-03-01 00:38:57 +0100

    Kirra lite skit

--- src/CheckOut.java
+++ src/CheckOut.java
@@ -1,20 +1,42 @@
+import java.awt.BorderLayout;
+import java.awt.CardLayout;
 import java.awt.EventQueue;
 
-import javax.swing.JDialog;
-import javax.swing.JButton;
-import java.awt.BorderLayout;
-import java.awt.event.MouseAdapter;
-import java.awt.event.MouseEvent;
-import java.awt.event.ActionListener;
-import java.awt.event.ActionEvent;
-import javax.swing.JPanel;
+import javax.swing.ButtonGroup;
 import javax.swing.GroupLayout;
+import javax.swing.JButton;
+import javax.swing.JDialog;
+import javax.swing.JFormattedTextField;
+import javax.swing.JPanel;
 import javax.swing.GroupLayout.Alignment;
 import javax.swing.LayoutStyle.ComponentPlacement;
-import java.awt.CardLayout;
+import javax.swing.JLabel;
+import javax.swing.SwingConstants;
+import java.awt.Font;
+import java.awt.Color;
+import javax.swing.border.LineBorder;
+import javax.swing.border.MatteBorder;
+import javax.swing.border.EtchedBorder;
+import javax.swing.border.SoftBevelBorder;
+import javax.swing.border.BevelBorder;
+import java.awt.SystemColor;
+import com.jgoodies.forms.layout.FormLayout;
+import com.jgoodies.forms.layout.ColumnSpec;
+import com.jgoodies.forms.layout.RowSpec;
+import com.jgoodies.forms.factories.FormFactory;
+import javax.swing.JRadioButton;
+import javax.swing.JTextField;
+import java.awt.event.ActionListener;
+import java.awt.event.ActionEvent;
 
 
 public class CheckOut extends JDialog {
+	private JTextField textField;
+	private JTextField textField_1;
+	private JTextField textField_2;
+	private JTextField textField_3;
+	private JTextField textField_4;
+	private JPanel panel_5;
 
 	/**
 	 * Launch the application.
@@ -37,32 +59,37 @@ public class CheckOut extends JDialog {
 	 * Create the dialog.
 	 */
 	public CheckOut() {
-		setBounds(100, 100, 456, 378);
+		setResizable(false);
+		setBounds(100, 100, 601, 483);
 		getContentPane().setLayout(new BorderLayout(0, 0));
 		
 		JPanel northPanel = new JPanel();
 		getContentPane().add(northPanel, BorderLayout.NORTH);
 		
 		JPanel panel = new JPanel();
+		panel.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
 		JPanel panel_1 = new JPanel();
+		panel_1.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
 		JPanel panel_2 = new JPanel();
+		panel_2.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		
 		JPanel panel_3 = new JPanel();
+		panel_3.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
 		GroupLayout gl_northPanel = new GroupLayout(northPanel);
 		gl_northPanel.setHorizontalGroup(
 			gl_northPanel.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_northPanel.createSequentialGroup()
-					.addGap(80)
+					.addGap(166)
 					.addComponent(panel, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(panel_1, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
 					.addComponent(panel_2, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
 					.addPreferredGap(ComponentPlacement.RELATED)
-					.addComponent(panel_3, GroupLayout.DEFAULT_SIZE, 135, Short.MAX_VALUE)
-					.addGap(80))
+					.addComponent(panel_3, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(226, Short.MAX_VALUE))
 		);
 		gl_northPanel.setVerticalGroup(
 			gl_northPanel.createParallelGroup(Alignment.LEADING)
@@ -75,23 +102,70 @@ public class CheckOut extends JDialog {
 						.addComponent(panel, GroupLayout.PREFERRED_SIZE, 65, GroupLayout.PREFERRED_SIZE))
 					.addContainerGap(GroupLayout.DEFAULT_SIZE, Short.MAX_VALUE))
 		);
+		panel_3.setLayout(new BorderLayout(0, 0));
+		
+		JLabel label_3 = new JLabel("4");
+		label_3.setFont(new Font("Tahoma", Font.BOLD, 32));
+		label_3.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(label_3, BorderLayout.CENTER);
+		
+		JLabel lblKvitto = new JLabel("Kvitto");
+		lblKvitto.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_3.add(lblKvitto, BorderLayout.SOUTH);
+		panel_2.setLayout(new BorderLayout(0, 0));
+		
+		JLabel label_2 = new JLabel("3");
+		label_2.setFont(new Font("Tahoma", Font.BOLD, 32));
+		label_2.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(label_2, BorderLayout.CENTER);
+		
+		JLabel lblBetalning = new JLabel("Betalning");
+		lblBetalning.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_2.add(lblBetalning, BorderLayout.SOUTH);
+		panel_1.setLayout(new BorderLayout(0, 0));
+		
+		JLabel label_1 = new JLabel("2");
+		label_1.setFont(new Font("Tahoma", Font.BOLD, 32));
+		label_1.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(label_1, BorderLayout.CENTER);
+		
+		JLabel lblLeverans = new JLabel("Leverans");
+		lblLeverans.setHorizontalAlignment(SwingConstants.CENTER);
+		panel_1.add(lblLeverans, BorderLayout.SOUTH);
+		panel.setLayout(new BorderLayout(0, 0));
+		
+		JLabel lblGranska = new JLabel("Granska");
+		lblGranska.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(lblGranska, BorderLayout.SOUTH);
+		
+		JLabel label = new JLabel("1");
+		label.setFont(new Font("Tahoma", Font.BOLD, 32));
+		label.setHorizontalAlignment(SwingConstants.CENTER);
+		panel.add(label, BorderLayout.CENTER);
 		northPanel.setLayout(gl_northPanel);
 		
 		JPanel panel_4 = new JPanel();
 		getContentPane().add(panel_4, BorderLayout.SOUTH);
 		
 		JButton btnBakt = new JButton("Bak\u00E5t");
+		btnBakt.setVisible(false);
 		
 		JButton btnNsta = new JButton("N\u00E4sta");
+		btnNsta.addActionListener(new ActionListener() {
+			public void actionPerformed(ActionEvent e) {
+				stekActionPerformed(e);
+			}
+		});
+		
 		GroupLayout gl_panel_4 = new GroupLayout(panel_4);
 		gl_panel_4.setHorizontalGroup(
 			gl_panel_4.createParallelGroup(Alignment.LEADING)
 				.addGroup(gl_panel_4.createSequentialGroup()
-					.addGap(80)
+					.addGap(166)
 					.addComponent(btnBakt)
-					.addPreferredGap(ComponentPlacement.RELATED, 145, Short.MAX_VALUE)
+					.addPreferredGap(ComponentPlacement.RELATED, 304, Short.MAX_VALUE)
 					.addComponent(btnNsta)
-					.addGap(80))
+					.addGap(166))
 		);
 		gl_panel_4.setVerticalGroup(
 			gl_panel_4.createParallelGroup(Alignment.LEADING)
@@ -103,9 +177,145 @@ public class CheckOut extends JDialog {
 		);
 		panel_4.setLayout(gl_panel_4);
 		
-		JPanel panel_5 = new JPanel();
+		panel_5 = new JPanel();
 		getContentPane().add(panel_5, BorderLayout.CENTER);
 		panel_5.setLayout(new CardLayout(0, 0));
+		
+		JPanel panel_6 = new JPanel();
+		panel_5.add(panel_6, "name_10081260351632");
+		
+		JPanel panel_7 = new JPanel();
+		panel_7.setBorder(new EtchedBorder(EtchedBorder.LOWERED, null, null));
+		panel_7.setBackground(Color.WHITE);
+		GroupLayout gl_panel_6 = new GroupLayout(panel_6);
+		gl_panel_6.setHorizontalGroup(
+			gl_panel_6.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_6.createSequentialGroup()
+					.addGap(50)
+					.addComponent(panel_7, GroupLayout.PREFERRED_SIZE, 499, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(46, Short.MAX_VALUE))
+		);
+		gl_panel_6.setVerticalGroup(
+			gl_panel_6.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel_6.createSequentialGroup()
+					.addComponent(panel_7, GroupLayout.DEFAULT_SIZE, 323, Short.MAX_VALUE)
+					.addContainerGap())
+		);
+		panel_6.setLayout(gl_panel_6);
+		
+		JPanel panel_8 = new JPanel();
+		panel_5.add(panel_8, "name_13325310248430");
+		
+		JPanel panel_9 = new JPanel();
+		GroupLayout gl_panel_8 = new GroupLayout(panel_8);
+		gl_panel_8.setHorizontalGroup(
+			gl_panel_8.createParallelGroup(Alignment.LEADING)
+				.addGroup(Alignment.TRAILING, gl_panel_8.createSequentialGroup()
+					.addContainerGap(157, Short.MAX_VALUE)
+					.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 304, GroupLayout.PREFERRED_SIZE)
+					.addGap(134))
+		);
+		gl_panel_8.setVerticalGroup(
+			gl_panel_8.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_8.createSequentialGroup()
+					.addContainerGap()
+					.addComponent(panel_9, GroupLayout.PREFERRED_SIZE, 172, GroupLayout.PREFERRED_SIZE)
+					.addContainerGap(151, Short.MAX_VALUE))
+		);
+		
+		JLabel lblFrnamn = new JLabel("F\u00F6rnamn:");
+		
+		textField = new JTextField();
+		textField.setColumns(10);
+		
+		JLabel lblEfternamn = new JLabel("Efternamn:");
+		
+		textField_1 = new JTextField();
+		textField_1.setColumns(10);
+		
+		JLabel lblAdress = new JLabel("Adress:");
+		
+		textField_2 = new JTextField();
+		textField_2.setColumns(10);
+		
+		textField_3 = new JTextField();
+		textField_3.setColumns(10);
+		textField_3.setDocument(new JTextFieldLimit(6));
+		
+		JLabel lblPostort = new JLabel("Postort:");
+		
+		textField_4 = new JTextField();
+		textField_4.setColumns(10);
+		
+		JLabel lblPostnummer = new JLabel("Postnummer:");
+		GroupLayout gl_panel_9 = new GroupLayout(panel_9);
+		gl_panel_9.setHorizontalGroup(
+			gl_panel_9.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_9.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_9.createParallelGroup(Alignment.LEADING)
+						.addGroup(gl_panel_9.createSequentialGroup()
+							.addGroup(gl_panel_9.createParallelGroup(Alignment.LEADING, false)
+								.addGroup(gl_panel_9.createSequentialGroup()
+									.addGroup(gl_panel_9.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblFrnamn)
+										.addComponent(textField, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE))
+									.addGap(18)
+									.addGroup(gl_panel_9.createParallelGroup(Alignment.LEADING)
+										.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, 130, GroupLayout.PREFERRED_SIZE)
+										.addComponent(lblEfternamn)))
+								.addComponent(lblAdress)
+								.addComponent(textField_2)
+								.addGroup(gl_panel_9.createSequentialGroup()
+									.addGap(44)
+									.addGroup(gl_panel_9.createParallelGroup(Alignment.LEADING)
+										.addComponent(lblPostnummer)
+										.addGroup(gl_panel_9.createSequentialGroup()
+											.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+											.addGap(18)
+											.addComponent(textField_4)))))
+							.addGap(16))
+						.addGroup(Alignment.TRAILING, gl_panel_9.createSequentialGroup()
+							.addComponent(lblPostort)
+							.addGap(107))))
+		);
+		gl_panel_9.setVerticalGroup(
+			gl_panel_9.createParallelGroup(Alignment.LEADING)
+				.addGroup(gl_panel_9.createSequentialGroup()
+					.addContainerGap()
+					.addGroup(gl_panel_9.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblFrnamn)
+						.addComponent(lblEfternamn))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_9.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_1, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addComponent(lblAdress)
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addComponent(textField_2, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+					.addPreferredGap(ComponentPlacement.UNRELATED)
+					.addGroup(gl_panel_9.createParallelGroup(Alignment.BASELINE)
+						.addComponent(lblPostort)
+						.addComponent(lblPostnummer))
+					.addPreferredGap(ComponentPlacement.RELATED)
+					.addGroup(gl_panel_9.createParallelGroup(Alignment.BASELINE)
+						.addComponent(textField_4, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE)
+						.addComponent(textField_3, GroupLayout.PREFERRED_SIZE, GroupLayout.DEFAULT_SIZE, GroupLayout.PREFERRED_SIZE))
+					.addContainerGap(19, Short.MAX_VALUE))
+		);
+		panel_9.setLayout(gl_panel_9);
+		panel_8.setLayout(gl_panel_8);
+		
+		ButtonGroup btngrp = new ButtonGroup();
+		
+		
 
+	}
+	
+	
+	public void stekActionPerformed(ActionEvent e) {
+		CardLayout cl = (CardLayout)panel_5.getLayout();
+		cl.next(panel_5);
 	}
 }
