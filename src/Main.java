0dba20797972f58d5e498a957b3b153cead63333
Christoffer Henriksson <christoffer_henriksson@hotmail.com>
2013-03-01 01:39:39 +0100

    Sparar IMatDataHandlers data vid avslut
    Förhindrar också utskrift under stängning

--- src/Main.java
+++ src/Main.java
@@ -1,3 +1,5 @@
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
 import java.io.FileNotFoundException;
 import java.io.PrintStream;
 
@@ -7,27 +9,43 @@ import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 
 public class Main {
+	
+	private static PrintStream defaultSystemOut = System.out;
 
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
-		// temporarily prevent output from IMatDataHandler
-		PrintStream defaultSystemOut = System.out;
-		try {
-			System.setOut(new PrintStream("bin/output.txt"));
-			IMatDataHandler.getInstance();
-		} catch (FileNotFoundException e) {
-			e.printStackTrace();
-		} finally {
-			System.setOut(defaultSystemOut);
-		}
+		// prevent output from IMatDataHandler at start
+		disableIMatDataHandlerOutput();
+		IMatDataHandler.getInstance(); // initialize
+		enableIMatDataHandlerOutput();
+		
+		// saves the current state of IMatDataHandler upon application exit
+		Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
+	        public void run() {
+	        	disableIMatDataHandlerOutput(); // prevent save logging
+	        	IMatDataHandler.getInstance().shutDown();
+	        }
+	    }));
 		
 		MainView mainView = new MainView();
 		mainView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainView.pack();
 		mainView.setLocationRelativeTo(null);
 		mainView.setVisible(true);
+	}
+	
+	private static void disableIMatDataHandlerOutput(){
+		try {
+			System.setOut(new PrintStream("bin/output.txt"));
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+	
+	private static void enableIMatDataHandlerOutput(){
+		System.setOut(defaultSystemOut);
 	}
 
 }
