99de38a2647d540f9883c34179483f6697501462
Christoffer Henriksson <christoffer_henriksson@hotmail.com>
2013-03-01 01:07:31 +0100

    Förhindrar utskrift från IMatDataHandler vid programmets start

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,9 @@
+import java.io.FileNotFoundException;
+import java.io.PrintStream;
+
 import javax.swing.JFrame;
+
+import se.chalmers.ait.dat215.project.IMatDataHandler;
 
 
 public class Main {
@@ -7,6 +12,17 @@ public class Main {
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// temporarily prevent output from IMatDataHandler
+		PrintStream defaultSystemOut = System.out;
+		try {
+			System.setOut(new PrintStream("bin/output.txt"));
+			IMatDataHandler.getInstance();
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		} finally {
+			System.setOut(defaultSystemOut);
+		}
+		
 		MainView mainView = new MainView();
 		mainView.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		mainView.pack();
